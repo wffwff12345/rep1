e12e7dd01c1f2b1de2852f7072097ca2ca16f919
wucp <2696982657@qq.com>
2022-08-12 16:13:05 +0900

    向本地添加springboot项目

---
+++ demo/src/com/test2/test.java
@@ -0,0 +1,11 @@
+package com.test2;
+
+public class test {
+  public static void main(String[] args) {
+    User user=new User();
+    user.setName("1");
+    user.setAge(1);
+    user.setName("2012-12-23");
+    System.out.println(user.toString());
+}
+}
